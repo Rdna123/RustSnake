decc4b254a9665b7b21a408e12760342dd1acc88
Rdna123 <chknmanrob1904@gmail.com>
2021-01-05 20:05:43 -0800

    Push for now.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
-use bevy::prelude::*;
+//#![windows_subsystem = "windows"]
+
 use bevy::input::system::exit_on_esc_system;
+use bevy::prelude::*;
 use bevy::render::pass::ClearColor;
 use rand::prelude::random;
 use std::time::Duration;
 
 fn main() {
     App::build()
-        .add_resource(WindowDescriptor{
+        .add_resource(WindowDescriptor {
             title: "Snake!".to_string(),
-            width: 500.0,
-            height: 500.0,
-            ..Default::default()
+            resizable: false,
+            width: 600.0,
+            height: 600.0,
+           ..Default::default()
         })
-
-        .add_resource(ClearColor(Color::rgb(0.04,0.04,0.04)))
+        .add_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_resource(SnakeMoveTimer(Timer::new(
             Duration::from_millis(150. as u64),
             true,
         )))
         .add_resource(SnakeSegments::default())
         .add_resource(LastTailPosition::default())
-
+        .add_resource(Points::default())
         .add_startup_system(setup.system())
-
         .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
-
         .add_event::<GrowthEvent>()
         .add_event::<GameOverEvent>()
-
+        .add_event::<ScoreEvent>()
         .add_system(snake_movement.system())
         .add_system(food_spawner.system())
         .add_system(position_translation.system())
@@ -36,13 +36,13 @@ fn main() {
         .add_system(size_scaling.system())
         .add_system(exit_on_esc_system.system())
         .add_system(snake_timer.system())
-        .add_system(food_body_check.system())
+        .add_system(food_overlap_check.system())
+        .add_system(point_score.system())
         .add_system(game_over.system())
-
         .add_plugins(DefaultPlugins)
+
         .run();
 }
-
 
 //SetUp
 struct Materials {
@@ -51,16 +51,16 @@ struct Materials {
     food_material: Handle<ColorMaterial>,
 }
 
-fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+fn setup(commands: &mut Commands,
+         mut materials: ResMut<Assets<ColorMaterial>>,
+){
     commands.spawn(Camera2dBundle::default());
     commands.insert_resource(Materials {
         head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
         segment_material: materials.add(Color::rgb(0.3, 0.3, 0.3).into()),
-        food_material: materials.add(Color::rgb(1.0, 0.0, 1.0).into()),
+        food_material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
     });
-
 }
-
 
 //Movement Logic
 #[derive(PartialEq, Copy, Clone)]
@@ -80,6 +80,9 @@ impl Direction {
             Self::Down => Self::Up,
         }
     }
+    fn scream(self){
+        println!("Rrrreeeeh")
+    }
 }
 
 struct SnakeMoveTimer(Timer);
@@ -92,6 +95,7 @@ fn snake_movement(
     keyboard_input: Res<Input<KeyCode>>,
     segments: ResMut<SnakeSegments>,
     snake_timer: ResMut<SnakeMoveTimer>,
+
     mut last_tail_position: ResMut<LastTailPosition>,
     mut game_over_events: ResMut<Events<GameOverEvent>>,
     mut heads: Query<(Entity, &mut SnakeHead)>,
@@ -103,69 +107,73 @@ fn snake_movement(
         .map(|e| *positions.get_mut(*e).unwrap())
         .collect::<Vec<Position>>();
 
+
     last_tail_position.0 = Some(*segment_positions.last().unwrap());
 
-    if let Some((head_entity, mut head)) = heads.iter_mut().next() {
-        let mut head_pos = positions.get_mut(head_entity).unwrap();
-        let dir: Direction = if keyboard_input.pressed(KeyCode::Left) {
-            Direction::Left
-        } else if keyboard_input.pressed(KeyCode::Down) {
-            Direction::Down
-        } else if keyboard_input.pressed(KeyCode::Up) {
-            Direction::Up
-        } else if keyboard_input.pressed(KeyCode::Right) {
-            Direction::Right
-        } else {
-            head.direction
-        };
-        if dir != head.direction.opposite() {
-            head.direction = dir;
-        }
-        if !snake_timer.0.finished() {
-            return;
-        }
-        match &head.direction {
-            Direction::Left => {
-                head_pos.x -= 1;
-            }
-            Direction::Right => {
-                head_pos.x += 1;
-            }
-            Direction::Up => {
-                head_pos.y += 1;
-            }
-            Direction::Down => {
-                head_pos.y -= 1;
-            }
-        }
-        if head_pos.x < 0
-            || head_pos.y < 0
-            || head_pos.x as u32 >= ARENA_WIDTH
-            || head_pos.y as u32 >= ARENA_HEIGHT
-        {
-            game_over_events.send(GameOverEvent);
-        }
-        if segment_positions.contains(&head_pos) {
-            game_over_events.send(GameOverEvent);
-        }
-    }
+        if let Some((head_entity, mut head)) = heads.iter_mut().next() {
+            let mut head_pos = positions.get_mut(head_entity).unwrap();
+
+            let dir: Direction = if keyboard_input.just_pressed(KeyCode::Left) {
+                Direction::Left
+            } else if keyboard_input.just_pressed(KeyCode::Down) {
+                Direction::Down
+            } else if keyboard_input.just_pressed(KeyCode::Up) {
+                Direction::Up
+            } else if keyboard_input.just_pressed(KeyCode::Right) {
+                Direction::Right
+            } else {
+                head.direction
+            };
 
 
 
-    segment_positions
-        .iter()
-        .zip(segments.0.iter().skip(1))
-        .for_each(|(pos, segment)| {
-            *positions.get_mut(*segment).unwrap() = *pos;
-        });
+            if dir.opposite() != head.direction{
+                head.direction = dir;
 
+            } else { dir.scream(); }
+
+            if !snake_timer.0.finished() {
+                return;
+            }
+            match &head.direction {
+                Direction::Left => {
+                    head_pos.x -= 1;
+                }
+                Direction::Right => {
+                    head_pos.x += 1;
+                }
+                Direction::Up => {
+                    head_pos.y += 1;
+                }
+                Direction::Down => {
+                    head_pos.y -= 1;
+                }
+            }
+            if head_pos.x < 0
+                || head_pos.y < 0
+                || head_pos.x as u32 >= ARENA_WIDTH
+                || head_pos.y as u32 >= ARENA_HEIGHT
+            {
+                game_over_events.send(GameOverEvent);
+            }
+            if segment_positions.contains(&head_pos) {
+                game_over_events.send(GameOverEvent);
+            }
+        }
+    segment_positions.iter().zip(segments.0.iter().skip(1)).for_each(|(pos, segment)| {
+        *positions.get_mut(*segment).unwrap() = *pos;
+    });
 }
-
 
 //Snake
-struct SnakeHead{
+
+struct SnakeHead {
     direction: Direction,
 }
+
+
+
+
 
 fn spawn_snake(
     commands: &mut Commands,
@@ -178,19 +186,13 @@ fn spawn_snake(
                 material: materials.head_material.clone(),
                 ..Default::default()
             })
-            .with(SnakeHead {
-                direction: Direction::Up,
-            })
+            .with(SnakeHead { direction: Direction::Up })
             .with(SnakeSegment)
             .with(Position { x: 3, y: 3 })
             .with(Size::square(0.8))
             .current_entity()
             .unwrap(),
-        spawn_segment(
-            commands,
-            &materials.segment_material,
-            Position { x: 3, y: 2 },
-        ),
+        spawn_segment(commands, &materials.segment_material, Position { x: 3, y: 2 }),
     ];
 }
 
@@ -205,10 +207,7 @@ fn spawn_segment(
     position: Position,
 ) -> Entity {
     commands
-        .spawn(SpriteBundle {
-            material: material.clone(),
-            ..Default::default()
-        })
+        .spawn(SpriteBundle { material: material.clone(), ..Default::default() })
         .with(SnakeSegment)
         .with(position)
         .with(Size::square(0.65))
@@ -216,12 +215,12 @@ fn spawn_segment(
         .unwrap()
 }
 
-
 //Eating and Growth
 fn snake_eating(
     commands: &mut Commands,
     snake_timer: ResMut<SnakeMoveTimer>,
     mut growth_events: ResMut<Events<GrowthEvent>>,
+    mut score_event: ResMut<Events<ScoreEvent>>,
     food_positions: Query<(Entity, &Position), With<Food>>,
     head_positions: Query<&Position, With<SnakeHead>>,
 ) {
@@ -233,6 +232,7 @@ fn snake_eating(
             if food_pos == head_pos {
                 commands.despawn(ent);
                 growth_events.send(GrowthEvent);
+                score_event.send(ScoreEvent);
             }
         }
     }
@@ -259,7 +259,6 @@ fn snake_growth(
     }
 }
 
-
 //Food
 struct Food;
 
@@ -276,7 +275,6 @@ fn food_spawner(
     time: Res<Time>,
     mut timer: Local<FoodSpawnTimer>,
 ) {
-
     if timer.0.tick(time.delta_seconds()).finished() {
         commands
             .spawn(SpriteBundle {
@@ -292,15 +290,15 @@ fn food_spawner(
     }
 }
 
-fn food_body_check (
+fn food_overlap_check(
     commands: &mut Commands,
-    segment_position: Query< &Position, With<SnakeSegment>>,
+    segment_position: Query<&Position, With<SnakeSegment>>,
     food_position: Query<(Entity, &Position), With<Food>>,
     head_position: Query<&Position, With<SnakeHead>>,
-){
-    for segment_pos in segment_position.iter(){
-        for head_pos in head_position.iter(){
-            for(ent, food_pos) in food_position.iter(){
+) {
+    for segment_pos in segment_position.iter() {
+        for head_pos in head_position.iter() {
+            for (ent, food_pos) in food_position.iter() {
                 if food_pos == segment_pos {
                     commands.despawn(ent);
                 }
@@ -311,7 +309,6 @@ fn food_body_check (
         }
     }
 }
-
 
 //size things
 
@@ -330,14 +327,9 @@ struct Size {
 }
 impl Size {
     pub fn square(x: f32) -> Self {
-        Self {
-            width: x,
-            height: x,
-        }
+        Self { width: x, height: x }
     }
 }
-
-
 
 fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Sprite)>) {
     let window = windows.get_primary().unwrap();
@@ -349,7 +341,10 @@ fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Sprite)>) {
     }
 }
 
-fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+fn position_translation(
+    windows: Res<Windows>,
+    mut q: Query<(&Position, &mut Transform)>,
+) {
     fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
         let tile_size = bound_window / bound_game;
         pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
@@ -364,7 +359,6 @@ fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Tra
     }
 }
 
-
 //GameOver
 struct GameOverEvent;
 
@@ -373,6 +367,7 @@ fn game_over(
     mut reader: Local<EventReader<GameOverEvent>>,
     game_over_events: Res<Events<GameOverEvent>>,
     materials: Res<Materials>,
+    mut total : ResMut<Points>,
     segments_res: ResMut<SnakeSegments>,
     food: Query<Entity, With<Food>>,
     segments: Query<Entity, With<SnakeSegment>>,
@@ -381,6 +376,41 @@ fn game_over(
         for ent in food.iter().chain(segments.iter()) {
             commands.despawn(ent);
         }
+        if total.score != 0 {
+            println!("Points for this round are {}", total.score);
+        }
+
+        total.score = 0;
         spawn_snake(commands, materials, segments_res);
+    }
+}
+
+struct ScoreEvent;
+struct Points{
+    score: i32,
+}
+
+impl Default for Points{
+    fn default() -> Self {
+       Self{
+           score: 0
+       }
+    }
+}
+
+fn point_score(
+   // commands: &mut Commands,
+    mut total : ResMut<Points>,
+    mut reader: Local<EventReader<ScoreEvent>>,
+    mut reader_game_over: Local<EventReader<GameOverEvent>>,
+    score_event: Res<Events<ScoreEvent>>,
+    game_over_event: Res<Events<GameOverEvent>>,
+){
+
+    if reader.iter(&score_event).next().is_some(){
+        total.score +=1;
+    }
+    if reader_game_over.iter(&game_over_event).next().is_some(){
+
     }
 }
